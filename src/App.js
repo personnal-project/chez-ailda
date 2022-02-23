19e142a45dcb64b364d5ab79bddebcb2c5e422e3
KevinMB22 <96531261+2C-Conseil@users.noreply.github.com>
2022-02-23 15:04:14 +0100

    Initialisation React

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './static/scss/App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'>
+        <div className='logo'><h1 className='logo-temp'> Chez Ailda</h1></div>
+          <div className='nav-items'>
+            <li>Accueil</li>
+            <li>Notre histoire</li>
+            <li>Carte</li>
+            <li>Contact</li>
+          </div>
+        <div className='Hamburger'></div>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
